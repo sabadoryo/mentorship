a47438ff068447e428ad3038fc9dee21705c35d7
Yernar Abilbay <yernar.abilbay@dataart.com>
2022-07-25 16:42:37 +0600

    Added learning-git ch

---
+++ learning-git/Eldashka.js
@@ -0,0 +1,17 @@
+class Hero {
+    gun = 'vandam';
+    armor = 151;
+    killCount = 0;
+
+    getHealth() {
+        return 100
+    }
+
+    getUltimateAbilityButton() {
+        return 'A';
+    }
+
+    incrementKillCount(killCount) {
+        this.armor++; 
+    }
+}
---
+++ learning-git/info.js
@@ -0,0 +1,33 @@
+const author = {
+    name: 'Yernar',
+    rank: 'radiant',
+    isTheBestPlayerInTheWorld: false
+}
+
+let students = [
+    {
+        name: 'Ildana',
+        age: 15,
+        sex: 'female'
+    },
+    {
+        name: 'Nursat',
+        valorantRank: 'iron',
+        age: 44,
+    },
+    {
+        name: 'Irana',
+        valorantRank: 'silver3',
+        mainCharacter: 'breach'
+    },
+    {
+        name: 'Zhenya',
+        hobby: 'Playing on a guitar',
+        favouriteAnime: 'Bakugan'
+    },
+    {
+        name: 'Adina',
+        isSageMain: false,
+        favouriteSkin: 'кальянный гост'
+    }
+]
