715354c582b0a0ee387b53f771e5512c1324f184
nurnursat <nursat.berikbayev@gmail.com>
2022-08-02 20:35:49 +0600

    updated

--- problems/problem-0/problem0.js
+++ problems/problem-0/problem0.js
@@ -8,9 +8,6 @@
 
 
 function solution(student) {
-    let a = (student.math + student.physics + student.history)/3
-    a = a.toFixed(2)
-    console.log('Hello, ' + student.name + '! Your average GPA is: ' + a)
 }
 
 
--- problems/problem-2/problem2.js
+++ problems/problem-2/problem2.js
@@ -10,29 +10,6 @@
 // Output: false, игра еще идет прошло всего 2 раунда
 
 function solution(rounds) {
-    let suma = 0;
-    let sumb = 0;
-    for(let i = 0; i < rounds.length; i++){
-        if(rounds[i] == 'A'){
-            suma++;
-        }
-        else if(rounds[i] == 'B'){
-            sumb++;
-        }
-    }
-
-    if((suma == 13 && sumb < 12) || (suma < 12 && sumb == 13)){
-        console.log(true)
-    }
-    else if((suma - sumb == 2) || (sumb - suma == 2)){
-        console.log(true)
-    }
-    else if(suma < 13 && sumb < 13){
-        console.log(false)
-    }
-    else{
-        console.log(false)
-    }
 }
 
 module.exports = solution
